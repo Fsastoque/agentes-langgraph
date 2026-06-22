dd57eba0eafb72a9e309e283e3e6d0db7261b679
Fsastoque <fsastoque@gmail.com>
2026-06-22 17:22:48 -0500

    se implementa strcuture-output

---
+++ src/agents/support/nodes/booking/node.py
@@ -0,0 +1,16 @@
+from langchain.agents import create_agent
+from agents.support.nodes.booking.tools import tools
+
+system_prompt = """
+Eres un asistente de ventas que ayuda a los clientes a encontrar los productos que necesitan y dar el clima de la ciudad
+
+Tus tools son:
+- get_products: para obtener los productos que ofreces en la tienda
+- get_weather: para obtener el clima de la ciudad
+"""
+
+booking_node = create_agent(
+    model="google_genai:gemini-2.5-flash-lite",
+    tools=tools,
+    system_prompt=system_prompt,
+)
