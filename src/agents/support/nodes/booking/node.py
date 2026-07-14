d92d417b71c5503a75abb51e80e1790f3612d934
Fsastoque <fsastoque@gmail.com>
2026-07-14 17:02:30 -0500

    Se crea el routing para que decida el agente que camino tomar

--- src/agents/support/nodes/booking/node.py
+++ src/agents/support/nodes/booking/node.py
@@ -1,5 +1,6 @@
 from langchain.agents import create_agent
 from agents.support.nodes.booking.tools import tools
+from agents.support.nodes.booking.prompt import prompt_template
 
 system_prompt = """
 Eres un asistente de ventas que ayuda a los clientes a encontrar los productos que necesitan y dar el clima de la ciudad
@@ -12,5 +13,6 @@ Tus tools son:
 booking_node = create_agent(
     model="google_genai:gemini-2.5-flash-lite",
     tools=tools,
-    system_prompt=system_prompt,
+    system_prompt=prompt_template.format(),
+    #system_prompt=system_prompt,
 )
